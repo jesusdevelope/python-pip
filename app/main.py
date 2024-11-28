ba0a0f28fa340a1127b2b0663ffb0b98c1d5ccf8
jesusdevelope <jesusromerodev.com@gmail.com>
2024-11-28 16:56:09 -0400

    using pandas

--- app/main.py
+++ app/main.py
@@ -10,12 +10,13 @@ def run():
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   '''
 
-  df = pd.read_csv('data.csv')
-  df = df[df['Continent'] == 'Africa']
-
-  countries = df['Country'].values
-  percentages = df['World Population Percentage'].values
-  charts.generate_pie_chart(countries, percentages)
+  df = pd.read_csv('data.csv') # Creación dataframe a partir de archivo .csc
+  df = df.loc[df['Continent'] == 'Europe',:] # Se filtran las filas cuyos valores de columna Continent sea igual a Europe
+  # o lo que sería lo mismo: df = df[df['Continent'] == 'Europe']
+  
+  countries = df['Country'].values #Se obtienen los valores de la columan Country del df
+  percentages = df['World Population Percentage'].values #Se obtienen los valores de la columna World Population Percentage
+  charts.generate_pie_chart(countries, percentages) # Sea cre gráfico de pie
 
   data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
